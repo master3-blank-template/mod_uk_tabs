eb4768ada49f674848edc091d0c3f6d7ca10c16c
AlekVolsk <alekvolsk@yandex.ru>
2019-02-22 20:32:56 +0400

    v1.2.0

--- mod_uk_tabs.php
+++ mod_uk_tabs.php
@@ -1,9 +1,11 @@
 <?php defined('_JEXEC') or die;
 /*
  * @package     mod_uk_tabs
- * @copyright   Copyright (C) 2018 Aleksey A. Morozov (AlekVolsk). All rights reserved.
+ * @copyright   Copyright (C) 2019 Aleksey A. Morozov (AlekVolsk). All rights reserved.
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+use Joomla\CMS\Helper\ModuleHelper;
 
 $vars = [
     'tabs_class', 'title_class', 'title_type', 'title_img_width', 'content_class', 'position', 'align',
@@ -11,8 +13,7 @@ $vars = [
     'items'
 ];
 
-foreach ($vars as $var)
-{
+foreach ($vars as $var) {
     $$var = $params->get($var);
 }
 
@@ -20,32 +21,34 @@ $module_id = $module->id;
 
 $tabs_class = trim($tabs_class) ? ' ' . trim($tabs_class) : '';
 $tabs_class = $position !== 'top' ? $tabs_class . ' uk-tab-' . $position : $tabs_class;
-switch ($align)
-{
-    case 'right': $tabs_class .= ' uk-flex-right'; break;
-    case 'center': $tabs_class .= ' uk-flex-center'; break;
-    case 'justify': $tabs_class .= ' uk-flex-between'; break;
-    case 'width': if ($position == 'top' || $position == 'bottom') $tabs_class .= ' uk-child-width-expand'; break;
+switch ($align) {
+    case 'right':
+        $tabs_class .= ' uk-flex-right';
+        break;
+    case 'center':
+        $tabs_class .= ' uk-flex-center';
+        break;
+    case 'justify':
+        $tabs_class .= ' uk-flex-between';
+        break;
+    case 'width':
+        if ($position == 'top' || $position == 'bottom') $tabs_class .= ' uk-child-width-expand';
+        break;
     case 'left':
-    default: ;
+    default:;
 }
 
 $tabs_params = [];
 $tabs_params[] = 'connect:#mod_uk_tabs_' . $module_id;
-if ((int)$active > 0 && (count((array)$items) > (int)$active))
-{
+if ((int)$active > 0 && (count((array)$items) > (int)$active)) {
     $tabs_params[] = 'active:' . $active;
-}
-else
-{
+} else {
     $active = 0;
 }
-if (!(bool)$swiping)
-{
+if (!(bool)$swiping) {
     $tabs_params[] = 'swiping:false';
 }
-if (trim($media))
-{
+if (trim($media)) {
     $tabs_params[] = 'media:' . trim($media);
 }
 $tabs_params = $tabs_params ? '="' . implode(';', $tabs_params) . '"' : '';
@@ -53,4 +56,6 @@ $tabs_params = $tabs_params ? '="' . implode(';', $tabs_params) . '"' : '';
 $title_class = trim($title_class) ? ' ' . trim($title_class) : '';
 $content_class = trim($content_class) ? ' ' . trim($content_class) : '';
 
-require(JModuleHelper::getLayoutPath('mod_uk_tabs', $params->get('layout', 'default')));
+if ($items) {
+    require(ModuleHelper::getLayoutPath('mod_uk_tabs', $params->get('layout', 'default')));
+}
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,8 +1,10 @@
 <?php defined('_JEXEC') or die;
 /*
  * @package     mod_uk_tabs
- * @copyright   Copyright (C) 2018 Aleksey A. Morozov (AlekVolsk). All rights reserved.
+ * @copyright   Copyright (C) 2019 Aleksey A. Morozov (AlekVolsk). All rights reserved.
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-require JModuleHelper::getLayoutPath('mod_uk_tabs', $params->get('layout', 'default') . '_' . $position);
+use Joomla\CMS\Helper\ModuleHelper;
+
+require ModuleHelper::getLayoutPath('mod_uk_tabs', $params->get('layout', 'default') . '_' . $position);
